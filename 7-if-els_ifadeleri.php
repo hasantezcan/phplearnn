b5481a354984ae5a9623b22267f049b5c6fd5472
hasantezcan <hasantezcan77@hotmail.com>
2018-01-20 00:22:35 +0300

    if else - else if ogrenildi..

---
+++ 7-if-els_ifadeleri.php
@@ -0,0 +1,26 @@
+<?php
+$islem = 2 + 2;
+
+if ($islem == 4){
+    echo "2 + 2 = 4 ...";
+  }
+
+echo "<br>";
+
+$benim_sayim= 5;
+
+if($benim_sayim == 4 )
+  echo "benim sayim 4!";
+else
+  echo "benim sayim 4 den farklıdır!";
+echo "<br>";
+$sayi_tut = 6;
+
+if($sayi_tut < 5)
+  echo "Sayım 5'den kucuk!";
+else if ($sayi_tut > 5)
+  echo "Sayım 5'den buyuk!";
+else
+  echo "Sayim 5!";
+echo "<br>";
+ ?>
