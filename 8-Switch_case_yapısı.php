33911076843461ca3f41424b7c5cc909fe1ec07e
hasantezcan <hasantezcan77@hotmail.com>
2018-01-20 00:46:28 +0300

    switch case yapısı ogrenildi..

---
+++ 8-Switch_case_yapısı.php
@@ -0,0 +1,35 @@
+<?php
+$secim = 3;
+
+switch ($secim){
+  case 1:
+    echo "Benim sayim 1!";
+    break;
+  case 2:
+    echo "Benim sayim 2!";
+    break;
+  case 3:
+    echo "Benim sayim 3!";
+    break;
+  case 4:
+    echo "Benim sayim 4!";
+  default:
+    echo  "Benim sayım 1,2 veya 3 degil!";
+}
+
+        echo "<br>";
+
+$cocuk = "kemal" /*"hasan"*/;
+
+switch ($cocuk) {
+  case "ali":
+    echo "cocuk alidir";
+    break;
+  case "hasan":
+    echo "cocuk hasandir";
+    break;
+  default:
+    echo "cocuk kimse degil...";
+}
+
+ ?>
