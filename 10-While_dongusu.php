c8b2f86e35a91c765d89a5ca3d94d75ed146dd8e
hasantezcan <hasantezcan77@hotmail.com>
2018-01-20 01:33:49 +0300

    While ve do while dongulerini ogrenildi..

---
+++ 10-While_dongusu.php
@@ -0,0 +1,22 @@
+<?php
+// while dongusu
+  $sayi = 1;
+
+  while ($sayi <= 5){
+    echo "Benim saym $sayi !";
+    echo "<br>";
+    $sayi++;
+  }
+
+echo "<br>";
+echo "<br>";
+//do while dongusu
+$sayi2 = 1;
+do{
+  $sayi2++;
+  echo "Benim sayimm $sayi2!";
+  echo "<br>";
+}
+while($sayi2 <= 10);
+
+ ?>
